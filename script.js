68f247776a713b32b69fd0cec84c2e6d6579ecb1
TawhidulIslam0 <147176853+TawhidulIslam0@users.noreply.github.com>
2025-09-20 14:54:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-// Placeholder functions for menu buttons
-function addR() {
-    alert("Add Row selected");
-}
-
-function addC() {
-    alert("Add Column selected");
-}
-
-function removeR() {
-    alert("Remove Row selected");
-}
-
-function removeC() {
-    alert("Remove Column selected");
-}
-
-function fillU() {
-    alert("Fill All Uncolored selected");
-}
-
-function fillAll() {
-    alert("Fill All selected");
-}
-
 function clearAll() {
-    alert("Clear selected");
-}
+  const table = document.getElementById('grid');
 
-function selectColor() {
-    alert("Color selected: " + document.getElementById("selectedColorId").value);
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      table.rows[i].cells[j].style.backgroundColor = "white";
+    }
+  }
 }
