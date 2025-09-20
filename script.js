3a0973d1a001d254da7a959d80a4f77ac1982e28
TawhidulIslam0 <147176853+TawhidulIslam0@users.noreply.github.com>
2025-09-20 14:49:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// Placeholder functions for menu buttons
-function addR() {
-    alert("Add Row selected");
-}
-
-function addC() {
-    alert("Add Column selected");
-}
-
-function removeR() {
-    alert("Remove Row selected");
-}
-
-function removeC() {
-    alert("Remove Column selected");
-}
-
-function fillU() {
-    alert("Fill All Uncolored selected");
-}
-
-function fillAll() {
-    alert("Fill All selected");
-}
-
-function clearAll() {
-    alert("Clear selected");
-}
-
 function selectColor() {
-    alert("Color selected: " + document.getElementById("selectedColorId").value);
+  colorSelected = document.getElementById("selectedColorId").value;
 }
