f664a1d0b002e64337b0d97cdf1b4d21e254ce1f
TawhidulIslam0 <147176853+TawhidulIslam0@users.noreply.github.com>
2025-09-20 14:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-// Placeholder functions for menu buttons
-function addR() {
-    alert("Add Row selected");
-}
+function initializeCell(cell) {
+  cell.style.backgroundColor = "white";
 
-function addC() {
-    alert("Add Column selected");
-}
-
-function removeR() {
-    alert("Remove Row selected");
-}
-
-function removeC() {
-    alert("Remove Column selected");
-}
-
-function fillU() {
-    alert("Fill All Uncolored selected");
-}
-
-function fillAll() {
-    alert("Fill All selected");
-}
-
-function clearAll() {
-    alert("Clear selected");
-}
-
-function selectColor() {
-    alert("Color selected: " + document.getElementById("selectedColorId").value);
+  cell.addEventListener("click", () => {
+    if (colorSelected && colorSelected !== "SELECT") {
+      cell.style.backgroundColor = colorSelected;
+    }
+  });
 }
